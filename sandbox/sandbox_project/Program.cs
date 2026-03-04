830870d7d240e27301c78e0816a8e756ac1f080a
sundazekiks <abutontravis1@gmail.com>
2026-03-04 17:34:38 -0500

    finished the arrays challenge

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -4,12 +4,17 @@ public class Program
 {
     static void Main(string[] args)
     {
-        var l1 = new[] { 1, 2, 3, 4, 5 };
-        var l2 = new[] { 2, 4, 6, 8, 10 };
-        var init = l1.Count() + l2.Count();
-        Console.WriteLine(init);
-        var result = new int[init];
+        List<int> d = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        var amount = 3;
+        List<int> lastElements = d.GetRange(d.Count - amount, amount);
+        d.RemoveRange(d.Count - amount, amount);
+        d.InsertRange(0, lastElements);
 
+        foreach (int i in d)
+        {
+            Console.WriteLine(i);
+        }
     }
+
 
 }
--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,21 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // Process:
+        // 1. Initialize a variable (List<double>) that will hold the multiple values
+        // 2. Loop through the length provided then 
+
+        // Initialize a list variable for storing our multiple values
+        List<double> multiples = new List<double>();
+
+        // Looping through the length
+        for (int i = 1; i <= length; i++)
+        {
+            // Using the iterator multiply it to the number value
+            multiples.Add(number * i);
+        }
+        // Since this function returns an array of doubles I converted my List to an Array 
+        return multiples.ToArray();
     }
 
     /// <summary>
@@ -29,5 +43,14 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Process: Getting the last elements first and store it in a list variable, so no matter how much the amount is we're just gonna get the last elements of the list, store it in list variable and then remove them from the the original list then insert it into the start of the list
+
+        // Get and Store the last elements from the data list in a variable
+        List<int> lastElems = data.GetRange(data.Count - amount, amount);
+        // Remove those last elements
+        data.RemoveRange(data.Count - amount, amount);
+        // then insert them into the start of the list
+        data.InsertRange(0, lastElems);
     }
 }
